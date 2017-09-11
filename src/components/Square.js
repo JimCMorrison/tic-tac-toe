6cbf5591ae492d8085f282d7a23e48bbe6af1a00
JimCMorrison <Jim@CodingMorrison.com>
2017-09-11 14:09:45 -0700

    Added Handled Click method on Board and updated Square component

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,16 +3,10 @@ import Game from '../components/Game';
 import Board from '../components/Board';
 
 export default class Square extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: null,
-    };
-  }
   render() {
     return (
-      <button className="square" onClick={() => this.setState({value: 'X'})}>
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
